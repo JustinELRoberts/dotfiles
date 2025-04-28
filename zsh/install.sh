a261f071e3975f1d9d36efc27b6bd4cbd8c8d1ae
jroberts329 <jroberts329@bloomberg.net>
2025-04-28 14:14:58 -0400

    Add ripgrep

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,3 +8,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 # zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# ripgrep
+brew install ripgrep
