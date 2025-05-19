1efcb8442735c85dc9b20b8c646983796c6dfc5b
jroberts329 <jroberts329@bloomberg.net>
2025-05-19 15:41:38 -0400

    Update bracket colors

--- nvim/lua/plugins/bracket-and-tab-highlighting.lua
+++ nvim/lua/plugins/bracket-and-tab-highlighting.lua
@@ -8,20 +8,18 @@ return {
   config = function()
     local highlight = {
       "RainbowYellow",
-      "RainbowBlue",
       "RainbowGreen",
+      "RainbowBlue",
       "RainbowViolet",
-      "RainbowCyan",
     }
     local hooks = require "ibl.hooks"
     -- create the highlight groups in the highlight setup hook, so they are reset
     -- every time the colorscheme changes
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
       vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
       vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
       vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
     end)
 
     require('rainbow-delimiters.setup').setup({ highlight = highlight })
