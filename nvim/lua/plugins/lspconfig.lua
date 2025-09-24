1247cd0802370d8479a31267616ee0261ae511ac
jroberts329 <jroberts329@bloomberg.net>
2025-09-24 16:56:55 -0400

    Update lsp settings for lua

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -46,7 +46,19 @@ return {
           lspformat.on_attach(client, bufnr)
           -- Custom macro to print() the active selection
           vim.fn.setreg("l", "yoprint('" .. esc .. "pa: ', " .. esc .. "pa);" .. esc .. "")
-        end
+        end,
+        settings = {
+          Lua = {
+            workspace = {
+              -- Make the server aware of Neovim runtime files
+              library = vim.api.nvim_get_runtime_file("", true),
+            },
+            diagnostics = {
+              -- Get the language server to recognize the 'vim' global
+              globals = { 'vim' },
+            },
+          },
+        },
       })
 
       lspconfig.ts_ls.setup({
